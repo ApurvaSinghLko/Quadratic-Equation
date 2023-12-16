babe2f217bb08d5938ec34cbbb9f205cf3ca8e32
Apurva Singh <88497948+ApurvaSinghLko@users.noreply.github.com>
2023-12-16 21:41:37 +0530

    Long live quadratic equation and meth!

---
+++ Meth.py
@@ -0,0 +1,23 @@
+import cmath
+
+
+a = float(input("Enter the value of a: "))
+
+b = float(input("Enter the value of b: "))
+
+c = float(input("Enter the value of c: "))
+
+
+# calculate the discriminant
+
+d = (b**2) - (4*a*c)
+
+
+# find two solutions
+
+cocaine = (-b-cmath.sqrt(d))/(2*a)
+
+marijuana = (-b+cmath.sqrt(d))/(2*a)
+
+
+print('The solution are {0} and {1}. Long live meth!'.format(cocaine,marijuana))
